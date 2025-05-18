693b05ce27ea6e399bec2b0d07971e04399af8e8
Hamed <hamed@niroomand.dev>
2025-05-18 19:37:25 +0330

    refactor: breakdown home page to components and implement dark mode

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -8,4 +8,38 @@ declare global {
 
 window.Alpine = Alpine;
 
+document.addEventListener('alpine:init', () => {
+  const initializeTheme = () => {
+    if (localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
+  initializeTheme();
+
+  window.addEventListener('toggle-theme', () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = 'light';
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }
+  });
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    if (!('theme' in localStorage)) {
+      if (event.matches) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    }
+  });
+});
+
 Alpine.start();
